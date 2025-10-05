967e4ade37300cd4dc0fcf0dfc7d9ed585ab194b
paul munywoki <paulmunywoki086@gmail.com>
2025-10-05 15:56:51 +0300

    Save local changes before pulling

--- main.py
+++ main.py
@@ -1511,4 +1511,4 @@ def init_db():
 
 if __name__ == '__main__':
     init_db()
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)), debug=False)
